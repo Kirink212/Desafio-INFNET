4f5c7545350f5776560754987447efa9961b6a3a
Luís Fernando Teixeira Bicalho <lfernandotexbicalho@hotmail.com>
2021-03-11 09:32:58 -0300

    Commiting to heroku

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/', router);
 
 // Listening to chosen PORT
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
